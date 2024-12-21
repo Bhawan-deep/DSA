ad4e70eba4d252fd82cdcb10987e9b588109bfca
Bhawandeep Singh <bhawandeepsingh976@gmail.com>
2024-12-21 22:44:38 +0530

    day 1 finished succesfully

---
+++ vardtypes.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class vardtypes{
+    public static void main (String args[]){
+        Scanner A= new Scanner(System.in);
+        Scanner B= new Scanner(System.in);
+       float a=A.nextFloat();
+        float b=B.nextFloat();
+        System.out.println(a+b);
+    
+}
+}
